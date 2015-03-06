92a753b728a0f3dbc117dc33e15c2c209ea17d75
Tim Kuminecz <tkuminecz@gmail.com>
2015-03-06 12:40:10 -0500

    add barebones main file

---
+++ src/main.js
@@ -0,0 +1,26 @@
+/* main.js */
+
+import http from 'jan';
+import q from 'q';
+
+export default {
+	getAssetList: function() {
+		
+	},
+	
+	getAsset: function(id) {
+		
+	},
+	
+	createAsset: function(opts) {
+		
+	},
+	
+	updateAsset: function(opts) {
+		
+	},	
+	
+	deleteAsset: function(id) {
+		
+	}
+};
