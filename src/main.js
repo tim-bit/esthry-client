8b1f17c79b67e4fb3eab2cb79d3ca9be8a6e1426
Tim Kuminecz <tkuminecz@gmail.com>
2015-03-25 14:45:49 -0400

    v0.0.3

--- src/main.js
+++ src/main.js
@@ -6,26 +6,109 @@ import q from 'q';
 class Esthry {
 	constructor(opts) {
 		this.opts = opts;
+
+		this.host = this.opts.host;
+		this.port = this.opts.port || '3000';
+		this.protocol = this.opts.protocol || 'http:';
 	}
 
+	// builds a url to the api
+	getUrl(path) {
+		return `${this.protocol}//${this.host}:${this.port}/${path}`;
+	}
+
+	// returns a list of asset IDs
 	getAssets() {
+		var defer = q.defer();
 
+		http.get(this.getUrl('asset/'), (err, res, data) => {
+			if (err) {
+				defer.reject(err);
+			}
+			else {
+				defer.resolve(data);
+			}
+		});
+
+		return defer.promise;
 	}
 
+	// returns a specific asset
 	getAsset(id) {
+		var defer = q.defer();
 
+		http.get(this.getUrl(`asset/${id}/`), (err, res, data) => {
+			if (err) {
+				defer.reject(err);
+			}
+			else {
+				defer.resolve(data);
+			}
+		});
+
+		return defer.promise;
 	}
 
+	// creates an asset according to `opts`
 	createAsset(opts) {
+		var defer = q.defer();
 
+
+		http.post({
+			url: this.getUrl(`asset/`)
+			body: JSON.stringify(opts)
+		}, (err, res, data) => {
+			if (err) {
+				defer.reject(err);
+			}
+			else {
+				defer.resolve(data);
+			}
+		});
+
+		return defer.promise;
 	}
 
+	// updates the asset specified in `opts`
 	updateAsset(opts) {
+		var defer = q.defer(),
+			id = opts.id;
 
+		delete opts.id;
+
+		http({
+			method: 'PUT',
+			url: this.getUrl(`asset/${id}`)
+			body: JSON.stringify(opts)
+		}, (err, res) => {
+			if (err) {
+				defer.reject(err);
+			}
+			else {
+				defer.resolve(res);
+			}
+		});
+
+		return defer.promise;
 	}
 
+	// deletes the given asset
 	deleteAsset(id) {
+		var defer = q.defer();
 
+		http({
+			method: 'DELETE',
+			url: this.getUrl(`asset/${id}/`)
+		}, (err, res) => {
+			if (err) {
+				defer.reject(err);
+			}
+			else {
+				defer.resolve(res);
+			}
+		});
+
+		return defer.promise;
 	}
 }
 
